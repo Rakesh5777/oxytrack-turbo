52bd8c2f4332aab374431915da9a89c328edde48
rakesh <rakeshreddy.k5777@gmail.com>
2024-03-01 23:45:32 +0530

    checking

--- apps/backend/src/routes/routes.ts
+++ apps/backend/src/routes/routes.ts
@@ -1,13 +1,11 @@
-import express from 'express';
-import masterUserRouter from './masterUser';
-import customerRouter from './customers/customersRouter';
-import { validateUser } from '../utils/middlewares';
+import express from "express";
+import masterUserRouter from "./masterUser";
+import customerRouter from "./customers/customersRouter";
+import { validateUser } from "../utils/middlewares";
 
 const rootRouter = express.Router();
 
 rootRouter.use("/masterUser", masterUserRouter);
 rootRouter.use("/customer", validateUser, customerRouter);
-
-
 
 export default rootRouter;
