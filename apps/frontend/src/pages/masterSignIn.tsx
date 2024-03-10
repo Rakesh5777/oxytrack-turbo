6bec4bcf9faeeb8e206ef16c655fc5ba60fc20ae
rakesh <rakeshreddy.k5777@gmail.com>
2024-03-10 16:13:48 +0530

    Update CSS and API contract files

--- apps/frontend/src/pages/masterSignIn.tsx
+++ apps/frontend/src/pages/masterSignIn.tsx
@@ -87,29 +87,43 @@ export function SignInForm() {
 }
 
 export function MasterSignIn() {
+  const [token] = useLocalStorage("token", "");
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (token) {
+      navigate("/");
+    }
+  }, [token, navigate]);
+
   return (
-    <div className="container relative h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 px-0">
-      <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-        <div className="absolute inset-0 bg-zinc-900" />
-        <div className="relative z-20 flex items-center text-lg font-medium">Medox Oxytrack</div>
-        <div className="relative z-20 mt-auto">
-          <blockquote className="space-y-2">
-            <p className="text-lg">
-              &ldquo;This application has saved me countless hours of work and helped me deliver oxygen to my clients faster than ever before.&rdquo;
-            </p>
-            <footer className="text-sm">Akhil kunda</footer>
-          </blockquote>
-        </div>
-      </div>
-      <div className="lg:p-8">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 w-full md:w-[350px]">
-          <div className="flex flex-col space-y-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">Signin to your account</h1>
-            <p className="text-sm text-muted-foreground">Enter your username and password</p>
+    <>
+      <div className="container relative h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 px-0">
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+          <div className="absolute inset-0 bg-zinc-900" />
+          <div className="relative z-20 flex items-center text-lg font-medium">Medox Oxytrack</div>
+          <div className="relative z-20 mt-auto">
+            <blockquote className="space-y-2">
+              <p className="text-lg">
+                &ldquo;This application has saved me countless hours of work and helped me deliver oxygen to my clients faster than ever
+                before.&rdquo;
+              </p>
+              <footer className="text-sm">Akhil kunda</footer>
+            </blockquote>
           </div>
-          <SignInForm />
+        </div>
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-screen p-8 md:p-0 flex-col justify-center space-y-6 lg:w-[400px]">
+            <div className="mx-auto w-5/6 lg:w-full">
+              <div className="flex flex-col space-y-2 text-center mb-6">
+                <h1 className="text-2xl font-semibold tracking-tight">Signin to your account</h1>
+                <p className="text-sm text-muted-foreground">Enter your username and password</p>
+              </div>
+              <SignInForm />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
