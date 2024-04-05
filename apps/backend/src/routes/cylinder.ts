7fdf465c3213b6d428d304cf8f4285e2aec1f922
rakesh <rakeshreddy.k5777@gmail.com>
2024-04-06 02:01:55 +0530

    Add getAllCylinders route handler

--- apps/backend/src/routes/cylinder.ts
+++ apps/backend/src/routes/cylinder.ts
@@ -1,10 +1,10 @@
 import Express from "express";
 import { asyncWrapper } from "../utils/middlewares";
-import { createCylinderHandler } from "../controllers/cylinder.controller";
+import { createCylinderHandler, getCylindersHandler } from "../controllers/cylinder.controller";
 
 const cylinderRouter = Express.Router();
 
 cylinderRouter.post("", asyncWrapper("createCylinder")(createCylinderHandler));
-// cylinderRouter.get("", asyncWrapper("getCylinders")(getCylindersHandler));
+cylinderRouter.get("", asyncWrapper("getAllCylinders")(getCylindersHandler));
 
 export default cylinderRouter;
